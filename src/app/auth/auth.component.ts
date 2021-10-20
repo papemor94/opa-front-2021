28d41650a503578d9230bc88edbd3e1b248238e4
Your Name <you@example.com>
2021-10-20 22:49:42 +0200

    back2

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -13,7 +13,7 @@ import {environment} from 'src/environments/environment'
 })
 export class AuthComponent implements OnInit {
 
-  authStatus : boolean= false; 
+  authStatus : boolean= false;
   constructor(private router : Router , private httpClient : HttpClient , private authService :AuthService) { }
 
   ngOnInit(): void {
@@ -24,49 +24,49 @@ export class AuthComponent implements OnInit {
 
 login(form : NgForm) {
 
-        let login  = form.value.login ; 
-        let password  = form.value.password ; 
+        let login  = form.value.login ;
+        let password  = form.value.password ;
         // log in
-        this.signin(login,password) ; 
+        this.signin(login,password) ;
         //
 
 
 }
 
 signOut(){
-  this.authStatus = this.authService.authStatus; 
-  console.log("Deconnexion")
-  console.log(this.authStatus); 
+  this.authStatus = this.authService.authStatus;
+  console.log("Deconnexion er")
+  console.log(this.authStatus);
 
 
 }
 signin(login, password){
 
-  // tentative de connexion pour l'utilisateur login , password 
+  // tentative de connexion pour l'utilisateur login , password
 
   var myHeaders = new Headers();
   myHeaders.append("Authorization", "Basic "+btoa(login+":"+password));
 
-  fetch("http://opaback:8091/android/auth", 
+  fetch("http://opaback:8091/android/auth",
   {
           method: 'POST',
           headers: myHeaders,
           redirect: 'follow'
   })
     .then(
-            response=>{ 
+            response=>{
               console.log(response.status , "status")
-              // on sauvegarde le token si tous se passe bien 
+              // on sauvegarde le token si tous se passe bien
               if (response.status==200){
                 response.text().then(
                     result=>{console.log(result)
-                    localStorage.setItem('token', result.substring(10,result.length-1))  
-                    localStorage.setItem('status', 'true')  
-                    this.authService.authStatus=true; 
+                    localStorage.setItem('token', result.substring(10,result.length-1))
+                    localStorage.setItem('status', 'true')
+                    this.authService.authStatus=true;
                     this.router.navigate(['users'])
             })
             .catch(error=>console.log('error',error))
-                  
+
             }
           else{
             alert(" login or password incorrect");
